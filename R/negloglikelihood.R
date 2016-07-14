b13dc1174cd303b84a6f2da9e40e9c3b5dd0a5a5
Francois Michonneau <francois.michonneau@gmail.com>
2016-07-14 10:18:02 -0400

    replace ... with named argument Q.ARGS

--- R/negloglikelihood.R
+++ R/negloglikelihood.R
@@ -5,13 +5,17 @@
 #' @param tip.values A data frame with at least two columns first column has tip labels in the same order than phylogenetic tree, second column ploidy or chromosome numbers depending on Q.FUN
 #' @param pi.0 Probability distribution to be used at the root of phylogeny
 #' @param Q.FUN A function that calculates the Q-matrix of the parameters of interest
-#' @param ... Extra parameters called in Q.FUN
+#' @param Q.ARGS A list of arguments to be passed to Q.FUN
 #' @return Result is the negative log-likelihood of parameters log.par given the model in Q.FUN
 #' @author Rosana Zenil-Ferguson
 #' @export
-negloglikelihood <- function(log.par, phy, tip.values, pi.0,Q.FUN, ...) {
-	Q.mat<-Q.FUN(log.par, ...) #Q.mat<-Q_bichrom(log.theta=log.params, size=max.chromosomes)
-	if (all(abs(rowSums(Q.mat)) < 1e-8)==TRUE){
+negloglikelihood <- function(log.par, phy, tip.values, pi.0, Q.FUN, Q.ARGS) {
+
+    if (!is.list(Q.ARGS)) stop("Q.ARGS must be a list")
+    Q.FUN <- match.fun(Q.FUN)
+    Q.mat <- do.call(Q.FUN, args = list(log.par, unlist(Q.ARGS)))
+
+    if (all(abs(rowSums(Q.mat)) < 1e-8)==TRUE){
 		print("Q matrix has all row sums equal to zero")
 	}else{
 		print("Q matrix not well defined. Rows don't add to zero")
